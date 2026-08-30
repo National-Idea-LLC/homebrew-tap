fd8c046908eed515d89c3a1f67a968d74c2b10ec
Elyas <info@elyas.sa>
2026-08-30 16:28:46 +0300

    Bump Squatter to 0.4.1 (#6)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- Casks/squatter.rb
+++ Casks/squatter.rb
@@ -1,6 +1,6 @@
 cask "squatter" do
-  version "0.4.0"
-  sha256 "e1dff7b0c6a16c9dd4be5482bcacf08db12830dad0427a22cfaf3794c26601a0"
+  version "0.4.1"
+  sha256 "c9506341fd58106809033120ff4c8b893d544dad90e98c50b981dce18f7c8beb"
 
   url "https://github.com/National-Idea-LLC/squatter/releases/download/v#{version}/Squatter-#{version}.dmg",
       verified: "github.com/National-Idea-LLC/squatter/"
